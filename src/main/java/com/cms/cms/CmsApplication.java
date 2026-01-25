f74ec93d402a3bea0f1a6df33b5d586eecf15845
parker0509 <167055018+parker0509@users.noreply.github.com>
2026-01-26 00:00:55 +0900

    feat : 예약 시스템 / 중복 테스트

--- src/main/java/com/cms/cms/CmsApplication.java
+++ src/main/java/com/cms/cms/CmsApplication.java
@@ -1,9 +1,11 @@
 package com.cms.cms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cms.cms.dao")
 public class CmsApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/cms/cms/CmsApplicationTests.java
+++ src/test/java/com/cms/cms/CmsApplicationTests.java
@@ -1,13 +1,41 @@
 package com.cms.cms;
 
+import com.cms.cms.dao.ReservationDao;
+import com.cms.cms.domain.Reservation;
+import com.cms.cms.service.ReservationService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 class CmsApplicationTests {
 
+	@Autowired
+	private ReservationService reservationService;
+
 	@Test
-	void contextLoads() {
+	void testCheckReservationTest(){
+
+		Reservation reservation = new Reservation();
+		reservation.setUserId(1L);
+		reservation.setResourceId(100L);
+		reservation.setStartDate(LocalTime.now());
+		reservation.setReservationDate(LocalDate.now());
+		reservation.setStartTime(LocalTime.of(10, 0));
+		reservation.setEndTime(LocalTime.of(11, 0));
+		reservation.setStatus("RESERVED");
+
+		Long count = reservationService.createReservation(reservation);
+
+		assertEquals(1,count);
+
 	}
 
 }
